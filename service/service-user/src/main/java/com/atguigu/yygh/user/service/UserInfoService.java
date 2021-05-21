17b07795ba38f7c2a6fdc0356f5e43cffd78ccf2
weijia <646025053@qq.com>
2021-05-21 11:58:21 +0800

    add user

--- service/service-user/src/main/java/com/atguigu/yygh/user/service/UserInfoService.java
+++ service/service-user/src/main/java/com/atguigu/yygh/user/service/UserInfoService.java
@@ -2,6 +2,10 @@ package com.atguigu.yygh.user.service;
 
 import com.atguigu.yygh.model.user.UserInfo;
 import com.atguigu.yygh.vo.user.LoginVo;
+import com.atguigu.yygh.vo.user.UserAuthVo;
+import com.atguigu.yygh.vo.user.UserInfoQueryVo;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 import java.util.Map;
@@ -10,4 +14,14 @@ public interface UserInfoService extends IService<UserInfo> {
     Map<String, Object> loginUser(LoginVo loginVo);
 
     UserInfo selectWxInfoOpenId(String openid);
+
+    void userAuth(Long userId, UserAuthVo userAuthVo);
+
+    IPage<UserInfo> selectPage(Page<UserInfo> pageParam, UserInfoQueryVo userInfoQueryVo);
+
+    void lock(Long userId, Integer status);
+
+    Map<String, Object> show(Long userId);
+
+    void approval(Long userId, Integer authStatus);
 }
--- service/service-user/src/main/java/com/atguigu/yygh/user/service/impl/UserInfoServiceImpl.java
+++ service/service-user/src/main/java/com/atguigu/yygh/user/service/impl/UserInfoServiceImpl.java
@@ -3,11 +3,16 @@ package com.atguigu.yygh.user.service.impl;
 import com.atguigu.yygh.common.ResultCodeEnum;
 import com.atguigu.yygh.common.exception.HospitalException;
 import com.atguigu.yygh.common.helper.JwtHelper;
+import com.atguigu.yygh.enums.AuthStatusEnum;
 import com.atguigu.yygh.model.user.UserInfo;
 import com.atguigu.yygh.user.mapper.UserInfoMapper;
 import com.atguigu.yygh.user.service.UserInfoService;
 import com.atguigu.yygh.vo.user.LoginVo;
+import com.atguigu.yygh.vo.user.UserAuthVo;
+import com.atguigu.yygh.vo.user.UserInfoQueryVo;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -24,12 +29,92 @@ public class UserInfoServiceImpl extends
     @Autowired
     private RedisTemplate<String ,String> redisTemplate;
 
+    //用户锁定
+    @Override
+    public void lock(Long userId, Integer status) {
+        if(status.intValue()==0 || status.intValue()==1) {
+            UserInfo userInfo = baseMapper.selectById(userId);
+            userInfo.setStatus(status);
+            baseMapper.updateById(userInfo);
+        }
+    }
+
+    @Override
+    public IPage<UserInfo> selectPage(Page<UserInfo> pageParam, UserInfoQueryVo userInfoQueryVo) {
+        //UserInfoQueryVo获取条件值
+        String name = userInfoQueryVo.getKeyword(); //用户名称
+        Integer status = userInfoQueryVo.getStatus();//用户状态
+        Integer authStatus = userInfoQueryVo.getAuthStatus(); //认证状态
+        String createTimeBegin = userInfoQueryVo.getCreateTimeBegin(); //开始时间
+        String createTimeEnd = userInfoQueryVo.getCreateTimeEnd(); //结束时间
+        //对条件值进行非空判断
+        QueryWrapper<UserInfo> wrapper = new QueryWrapper<>();
+        if(!StringUtils.isEmpty(name)) {
+            wrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(status)) {
+            wrapper.eq("status",status);
+        }
+        if(!StringUtils.isEmpty(authStatus)) {
+            wrapper.eq("auth_status",authStatus);
+        }
+        if(!StringUtils.isEmpty(createTimeBegin)) {
+            wrapper.ge("create_time",createTimeBegin);
+        }
+        if(!StringUtils.isEmpty(createTimeEnd)) {
+            wrapper.le("create_time",createTimeEnd);
+        }
+        //调用mapper的方法
+        IPage<UserInfo> pages = baseMapper.selectPage(pageParam, wrapper);
+        //编号变成对应值封装
+        pages.getRecords().stream().forEach(item -> {
+            this.packageUserInfo(item);
+        });
+        return pages;
+    }
+    //编号变成对应值封装
+    private UserInfo packageUserInfo(UserInfo userInfo) {
+        //处理认证状态编码
+        userInfo.getParam().put("authStatusString",AuthStatusEnum.getStatusNameByStatus(userInfo.getAuthStatus()));
+        //处理用户状态 0  1
+        String statusString = userInfo.getStatus().intValue()==0 ?"锁定" : "正常";
+        userInfo.getParam().put("statusString",statusString);
+        return userInfo;
+    }
+
+    //    用户认证
+    @Override
+    public void userAuth(Long userId, UserAuthVo userAuthVo) {
+        //根据用户id查询用户信息
+        UserInfo userInfo = baseMapper.selectById(userId);
+        //设置认证信息
+        //认证人姓名
+        userInfo.setName(userAuthVo.getName());
+        //其他认证信息
+        userInfo.setCertificatesType(userAuthVo.getCertificatesType());
+        userInfo.setCertificatesNo(userAuthVo.getCertificatesNo());
+        userInfo.setCertificatesUrl(userAuthVo.getCertificatesUrl());
+        userInfo.setAuthStatus(AuthStatusEnum.AUTH_RUN.getStatus());
+        //进行信息更新
+        baseMapper.updateById(userInfo);
+    }
+
     @Override
     public UserInfo selectWxInfoOpenId(String openid) {
         QueryWrapper<UserInfo> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("openid",openid);
         UserInfo userInfo = baseMapper.selectOne(queryWrapper);
         return userInfo;
+    }
+
+    @Override
+    public Map<String, Object> show(Long userId) {
+        return null;
+    }
+
+    @Override
+    public void approval(Long userId, Integer authStatus) {
+
     }
 
     @Override
